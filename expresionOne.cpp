5eed3eed23fadf082165193ca20114c6a43cc30b
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-19 09:57:42 -0400

    utilizando namespace std

--- expresionOne.cpp
+++ expresionOne.cpp
@@ -2,21 +2,26 @@
 // A = 2 + B + C
 
 #include<iostream>
+using namespace std;
 
 int main(){
     int valorB;
     int valorC;
     int valorA_resultado;
 
-    std::cout<<"Ingrese el primer valor";
-    std::cin>>valorB;
+    //std::cout<<"Ingrese el primer valor ";
+    cout<<"Ingrese el primer valor ";
+    //std::cin>>valorB;
+    cin >> valorB;
 
-    std::cout<<"Ingrese el segundo valor";
-    std::cin>>valorC;
+    //std::cout<<"Ingrese el segundo valor ";
+    cout <<"Ingrese el segundo valor ";
+    //std::cin>>valorC;
+    cin >> valorC;
 
     valorA_resultado = 2 + valorB + valorC;
 
-    std::cout<<" 2 " + " + "+valorB+" + "+valorC+" = ";
-
+    //std::cout<< "2 + "<<valorB <<" + " <<valorC<< " = "<< valorA_resultado << std::endl;
+    cout<< "2 + "<<valorB <<" + " <<valorC<< " = "<< valorA_resultado << std::endl;
     return 0;
 }
